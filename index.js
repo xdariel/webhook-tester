10ca84b095dff4e11538c2c76df3306b0e25c11d
xdariel <dariel87@gmail.com>
2023-10-05 12:31:39 -0400

    Update webhook testing

--- index.js
+++ index.js
@@ -7,23 +7,35 @@ const {join} = require('path')
 const PORT = process.env.PORT ?? 3000;
 const LOGS_PATH = process.env.LOGS_PATH ?? "./logs"
 
+const convertURI=(uri)=> {
+    if (typeof uri === "string") {
+        return JSON.parse(uri)
+    } else {
+        return uri
+    }
+}
+
 const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.post('/webhook', (req, res,) => {
+
+app.post('/webhook/:app', (req, res,) => {
     try {
+
+        const app = req.params.app
         const body = req.body
         const filePath = join(LOGS_PATH, `${Date.now()}-webhook.json`)
 
         const toSave = {
             type: typeof body,
+            app,
             body,
             date: new Date()
         }
 
         console.log("---------------------body-----------------------")
         console.log("TYPE: ", typeof body)
-        console.log(body)
+        console.log(convertURI(body))
         console.log("---------------------body-----------------------")
 
         fs.writeFile(filePath, JSON.stringify(toSave), 'utf8', (err) => {
